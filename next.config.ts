fc5b847095603b23e542b0d637bcf8e1cb502a1e
Seishu Otori <seishu@SeishunoMacBook-Air.local>
2025-08-03 04:11:31 +0900

    add 404 error

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,26 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(js|ts|tsx)$/,
+      use: {
+        loader: require.resolve('string-replace-loader'),
+        options: {
+          search: 'from\\s+[\'"]next/document[\'"]',
+          replace: (match) => {
+            console.log('Detected import from next/document');
+            return match;
+          },
+          flags: 'g',
+        },
+      },
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
